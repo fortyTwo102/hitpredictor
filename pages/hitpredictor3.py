5269c16e9d76a2f130ca0bdc47f4f685e3836c0a
farooq <farooq73a@gmail.com>
2019-03-20 15:25:20 +0530

    without  dataset

--- pages/hitpredictor3.py
+++ pages/hitpredictor3.py
@@ -5,13 +5,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
 
-
-import math
-
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
-
-
 database = {}
 
 trackFetched = "" 
@@ -132,57 +125,38 @@ def Predict(feature):
 		artistFetched = feature[-1]
 		feature = feature[:-2]
 	
-	'''dirpath = os.path.dirname(os.path.abspath(__file__))
-			
-				data = np.loadtxt(dirpath + "\\songdatabase1.txt", delimiter='\t')
-				
-				#Removing Outliers
-				z = np.abs(stats.zscore(data))
-				data = data[(z < 3).all(axis=1)]
-			
-				X, y = data[:, :16], data[:, 16]
-			
-				mean, sigma, X = featureNorm(X)'''
+	dirpath = os.path.dirname(os.path.abspath(__file__))
+
+	data = np.loadtxt(dirpath + "\\songdatabase1.txt", delimiter='\t')
+	
+	#Removing Outliers
+	z = np.abs(stats.zscore(data))
+	data = data[(z < 3).all(axis=1)]
+
+	X, y = data[:, :16], data[:, 16]
+
+	mean, sigma, X = featureNorm(X)
 
 	feature = np.array(feature)
-
-	mean = [57.625, 0.5835781324582339, 0.654001223150358, 5.230608591885441, -7.460534307875895, 0.6781026252983293, 0.08133218973747017, 0.21211613053400952, 0.1537536808711217, 0.16788397971360383, 0.4597849940334129, 119.74174433174225, 238704.5886038186, 3.9388424821002386, 40.00194386933174, 10.279534606205251]
-	sigma = [21.518846328626353, 0.1752319536831673, 0.21679968258791865, 3.5672343581732098, 3.494355651030302, 0.4672038686289347, 0.07525371619350751, 0.26861114395875435, 0.30849449096614123, 0.11428926622921333, 0.2520444019009235, 28.655790678789145, 63031.36109581164, 0.2819461796498358, 16.42316566194086, 2.849291043273701]
-
 	mean = np.array(mean)
 	sigma = np.array(sigma) 
-
 
 	feature = (feature - mean) / sigma #getting the scaled feature
 	feature = feature.reshape(-1,feature.shape[0])
 
 	#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=1)
 
-	'''model = LogisticRegression(solver='lbfgs')
+	model = LogisticRegression(solver='lbfgs')
+
+	model.fit(X, y)
+
+	#feature = feature.reshape(-1,feature.shape[0])
 
 	
 
-	a = model.fit(X, y)'''
+	y_pred = model.predict(feature)
 
-	intercept = np.array([0.94735381])
-	#feature = feature.reshape(-1,feature.shape[0])
-
-	trained = [0.68730573,0.57614029,-0.61779822,0.09654665,0.42180564,-0.04661171,-0.19334932,-0.51271454,-1.24322112,0.03067779,0.6676387,0.11899211,0.35745331,0.10556957,-0.11559462,0.1769944]
-
-	trained = np.array(trained)
-
-	feature = feature[0]
-
-	perc = sigmoid(np.dot(feature, trained) + intercept)
-
-
-	#print(a.coef_)
-	#print(feature)
-
-	'''y_pred = model.predict(feature)'''
-
-	#probability = round((model.predict_proba(feature)[0][1])*100,1)
-	probability = round(perc*100,1)
+	probability = round((model.predict_proba(feature)[0][1])*100,1)
 	#print("There's a",probability,"% chance of "+trackFetched + " by "+artistFetched+ " being a hit!")
 
 	return probability, trackFetched, artistFetched
